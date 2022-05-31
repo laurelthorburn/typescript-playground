da48db1b1a941654940cb9ae4b3a8566ccbcf755
Laurel Thorburn <codinglaurel@gmail.com>
2022-05-31 12:26:35 -0600

    Completed exercise 1

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -25,9 +25,9 @@ let user: [number, string] = [1, 'Mosh'];
 
 //enums
 //PascalCase
-const enum Size{ Small = 0, Medium, Large };
+const enum Size { Small = 0, Medium, Large };
 // will implicitly assign M and L
-const enum Size1 { Small = 's', Medium = 'm', Large ='l'}
+const enum Size1 { Small = 's', Medium = 'm', Large = 'l' }
 // bc using string have to explicity write out M and L
 
 //define using const = compiler generates more optimized code
@@ -46,14 +46,39 @@ const enum Size1 { Small = 's', Medium = 'm', Large ='l'}
 function calculateTax(income: number, taxYear = 2022): number {
     if ((taxYear) < 2022)
         return income * 1.2;
-return income * 1.3;
+    return income * 1.3;
 }
 
 calculateTax(10_000, 2022)
 
 //Objects
 let employee: {
-    id: number,
-    name?: string
-} = { id: 1 };
-employee.name = 'Mosh';
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = { id: 1, name: 'Mosh', retire: (date: Date) => console.log(date) };
+//readonly prtects id, keeps us from overwriting it
+
+// Exercises•
+
+//For each of these values, what type will the TypeScript compiler infer? 
+let a = 100; //number
+let b = 'Coffee'; //string
+let c = [true, false, false]; //array of booleans (boolean[])
+ let d = {age:number}; //object a which is a number ({age: 31})
+ let e = [3]; //array of number (number[])
+ let f; //any
+ let g = []; //array of any (any[])
+
+//  •What are the compilation errors in each of the following code snippets?
+
+let song: {
+    title:string,
+    releaseYear:number
+} = { title:'My song'}; //missing releaseYear
+
+let prices = [100, 200, 300];
+prices[0] = '$100'; //pushing a string to a number
+
+functionmyFunc(a:number, b:number):number { }
+//you have an empty function with unused parameters and no return, fctn never used either
